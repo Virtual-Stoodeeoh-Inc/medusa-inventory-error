406051afaa37e49989d730de72494de63edfb97b
virtualstoodeeoh-dev <ceebti@gmail.com>
2025-01-22 07:46:15 -0500

    Reservation Deletation Subscription

---
+++ src/subscribers/order-fulfillment-created.ts
@@ -0,0 +1,43 @@
+import { SubscriberArgs, type SubscriberConfig } from "@medusajs/framework";
+import { ContainerRegistrationKeys, Modules } from "@medusajs/framework/utils";
+
+export default async function orderFulfillmentCreatedHandler({
+  event: { data },
+  container,
+}: SubscriberArgs<{ order_id: string }>) {
+  const query = container.resolve(ContainerRegistrationKeys.QUERY);
+  const inventoryModuleService = container.resolve(Modules.INVENTORY);
+
+  const { data: orders } = await query.graph({
+    entity: "order",
+    fields: [
+      "id",
+      "items.id",
+      "items.quantity",
+      "items.variant.id",
+      "items.variant.inventory_items.id",
+    ],
+    filters: {
+      id: data.order_id,
+    },
+  });
+
+  const order = orders[0];
+  if (!order) {
+    return;
+  }
+
+  for (const item of order.items) {
+    const reservations = await inventoryModuleService.listReservationItems({
+      line_item_id: item.id,
+    });
+
+    for (const reservation of reservations) {
+      await inventoryModuleService.deleteReservationItems(reservation.id);
+    }
+  }
+}
+
+export const config: SubscriberConfig = {
+  event: `order.fulfillment_created`,
+};
